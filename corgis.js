e90528db63b61d547923b826b77dc451f3db1638
Shawn Tabai <stabai@gmail.com>
2019-04-19 18:37:51 -0700

    Add cats, more dogs, and on demand flipping.

--- corgis.js
+++ corgis.js
@@ -1,27 +1,48 @@
-var CORGI_TYPES = ['choco', 'oreo', 'velvet'];
+var ANIMALS = [
+	{ 'type': 'cat', 'imageUrl': 'cat-calico.gif' },
+	{ 'type': 'cat', 'imageUrl': 'cat-midnight.gif' },
+	{ 'type': 'cat', 'imageUrl': 'cat-noogler.gif' },
+	{ 'type': 'cat', 'imageUrl': 'cat-orange.gif' },
+	{ 'type': 'cat', 'imageUrl': 'cat-siamese.gif' },
+	{ 'type': 'cat', 'imageUrl': 'cat-stripes.gif' },
+	{ 'type': 'corgi', 'imageUrl': 'corgi-choco-xmas.gif' },
+	{ 'type': 'corgi', 'imageUrl': 'corgi-choco.gif' },
+	{ 'type': 'corgi', 'imageUrl': 'corgi-oreo-xmas.gif' },
+	{ 'type': 'corgi', 'imageUrl': 'corgi-oreo.gif' },
+	{ 'type': 'corgi', 'imageUrl': 'corgi-velvet-xmas.gif' },
+	{ 'type': 'corgi', 'imageUrl': 'corgi-velvet.gif' },
+];
 
 const codeMirror = CodeMirror.fromTextArea($('#code').get(0), {
-  lineNumbers: true,
-  matchBrackets: true,
-  mode: "text/javascript"
+	lineNumbers: true,
+	matchBrackets: true,
+	mode: "text/javascript"
 });
 
-function corgis() {
+function animals() {
 	const $header = $('header');
-	const $corgi = $('<div class="corgi">');
-	const corgiType = CORGI_TYPES[getRandomInt(CORGI_TYPES.length)];
-	$corgi.addClass(corgiType);
+	const $animal = $('<div class="animal">');
+	const animal = ANIMALS[getRandomInt(ANIMALS.length)];
+
+	$animal.addClass(animal.type);
+	$animal.css('background-image', `url(${animal.imageUrl})`);
+
+	const flipped = getRandomInt(2) === 1;
+	const cssProperty = flipped ? 'left' : 'right';
+	if (flipped) {
+		$animal.addClass('flipped');
+	}
 
 	const transitionDuration = getRandomInt(21) + 5;
-	$corgi.css('transition-duration', transitionDuration + 's');
+	$animal.css('transition-duration', transitionDuration + 's');
 
 	const targetValue = $header.width() + 131 + 'px';
-	const cssProperty = (corgiType === 'oreo') ? 'left' : 'right';
-	$header.append($corgi);
-	setTimeout(function() {
-		$corgi.css(cssProperty, targetValue);
-		setTimeout(function() {
-			$corgi.remove();
+
+	$header.append($animal);
+	setTimeout(function () {
+		$animal.css(cssProperty, targetValue);
+		setTimeout(function () {
+			$animal.remove();
 		}, transitionDuration * 1000);
 	}, 1);
 }
@@ -30,18 +51,18 @@ function getRandomInt(max) {
 	return Math.floor(Math.random() * Math.floor(max));
 }
 
-codeMirror.on('change', function() {
-	if (getRandomInt(5) === 0) {
-		corgis();
+codeMirror.on('change', function () {
+	if (getRandomInt(3) === 0) {
+		animals();
 	}
 	localStorage.setItem('code', codeMirror.getValue());
 });
 
 function initialize() {
-	let code = localStorage.getItem('code') || corgis.toString();
+	let code = localStorage.getItem('code') || animals.toString();
 	codeMirror.replaceSelection(code);
 	codeMirror.focus();
-	corgis();
+	animals();
 }
 
 initialize();
